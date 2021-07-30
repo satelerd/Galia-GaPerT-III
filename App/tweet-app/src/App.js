6aa922ce04e94aec2300e1ed046f03cd70acbfde
Daniel <satelerd@gmail.com>
2021-07-30 14:27:36 -0400

    buttons work

--- App/tweet-app/src/App.js
+++ App/tweet-app/src/App.js
@@ -2,29 +2,13 @@ import React, { useState } from "react";
 import './App.css';
 import Card from "./components/card/Card"
 
-var Twitter = require('twitter');
-
-const cliente = new Twitter({
-    consume_key:"ulIyy3tdHD2ICYAQuLfmaPFY3",
-    consumer_secret:"pF63JdlSd8mYIsWLP4KVnoZG8DIRRP9HsEv2nfMZ1FVHKQZVdJ",
-    access_token: "1419713857514311681-pIrCxJOHrWk2mOXtYTjL4zzuRSnxbS",
-    access_token_secret: "y3gueu71x60uIvnHOxZc6okYWpBmijoiTHFOwUgBUPjx9"
-})
-
-cliente.post("statuses/update", {status: "pruebaa"})
-    .then((tweet) => {
-        console.log(tweet);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
 
 function App() {
   const [theme, setTheme] = useState();
 
   var title = "Elige el tema para el tweet"
   var note = "(Dejalo vacio para que Galia genere su propio tema)"
-  var posibleInput = [{html: <input onChange={ event =>setTheme(event.target.value)}></input>}, {html: <div></div>}]
+  var posibleInput = [{html: <input placeholder="Ej: La humanidad" onChange={ event =>setTheme(event.target.value)}></input>}, {html: <div></div>}]
   var buttons = {button: <button>Generar Tweet</button>, button1: <button>Twittear</button>, button2: <button>Generar de nuevo</button>}
 
   return (
--- App/tweet-app/src/components/card/Card.js
+++ App/tweet-app/src/components/card/Card.js
@@ -9,11 +9,14 @@ function Card({title, note, theme, tweet, isinput, button, button1, button2}) {
             <div className="card-body text-center">
                 <h4 className="card-title">{title}</h4>
                 <h5 className="card-text">{note}</h5>
-                {isinput}
-                {button}
-
-                {button1}
-                {button2}
+                <div className="card-bot">
+                    {isinput}
+                    {button}
+                </div>
+                <div className="card-buttons">
+                    {button1}
+                    {button2}
+                </div>
             </div>
         </div>
     ) 
