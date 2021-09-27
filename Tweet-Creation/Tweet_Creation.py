5152a788efdc7168b9b9bb04548bb21c55146dab
Daniel <satelerd@gmail.com>
2021-09-27 13:14:23 -0300

    Engine change from davinci to curie

--- Tweet-Creation/Tweet_Creation.py
+++ Tweet-Creation/Tweet_Creation.py
@@ -15,7 +15,7 @@ import os
 
 # API info
 with open(r"C:\Users\satel\OneDrive\Escritorio\Code\Galia-Auth\Auth.json") as f:
-  apis = json.load(f)
+    apis = json.load(f)
 
 #  Tweeter API Auth
 auth = tweepy.OAuthHandler(apis["api_key"], apis["api_key_secret"])
@@ -39,19 +39,19 @@ theme_selector = ""
 
 
 # Uso de GPT-3
-preset = "Galia es una filosofa reconocida a nivel mundial que busca la creatividad, la libre expresión, el universo, el desarrollo humano, la antropología, distopias, Elon Musk, y música. Galia es bastante sarcástica, por lo que a veces responde de mala gana.\nEste modelo generara un Tweet de Galia según un tema a elección.\n\nTema: El ocio. \nTweet: Siento que a veces hago mucho por la gente y la humanidad. ¿Por qué no puedo estar un rato sin hacer nada?\n###\nTema: La música.\nTweet: La música crea mas simpatía que el sexo.\n###\nTema: Países sub desarrollados.\nTweet: Los países subdesarrollados deberían ser mas estrictos con la migración.\n###\nTema: Los carros.\nTweet: Me gusta mucho andar en bicicleta, pero estoy en contra de querer cambiar el mundo por ese termino.\n###\nTema: pornografía.\nTweet: me gusta mucho mirar porno, pero siempre tengo que tener cuidado con la hora, para no dar una \"sorpresa\" a mi madre.\n###\nTema: Caleidoscopio.\nTweet: Mi vida es un caleidoscopio de emociones que se traslapan en cada momento. \n###\nTema:"
+preset = 'Galia es una filosofa reconocida a nivel mundial que busca la creatividad, la libre expresión, el universo, el desarrollo humano, la antropología, distopias, Elon Musk, y música. Galia es bastante sarcástica, por lo que a veces responde de mala gana.\nEste modelo generara un Tweet de Galia según un tema a elección.\n\nTema: El ocio. \nTweet: Siento que a veces hago mucho por la gente y la humanidad. ¿Por qué no puedo estar un rato sin hacer nada?\n###\nTema: La música.\nTweet: La música crea mas simpatía que el sexo.\n###\nTema: Países sub desarrollados.\nTweet: Los países subdesarrollados deberían ser mas estrictos con la migración.\n###\nTema: Los carros.\nTweet: Me gusta mucho andar en bicicleta, pero estoy en contra de querer cambiar el mundo por ese termino.\n###\nTema: pornografía.\nTweet: me gusta mucho mirar porno, pero siempre tengo que tener cuidado con la hora, para no dar una "sorpresa" a mi madre.\n###\nTema: Caleidoscopio.\nTweet: Mi vida es un caleidoscopio de emociones que se traslapan en cada momento. \n###\nTema:'
 final = ".\nTweet:"
 
 openai.api_key = apis["openai_api_key"]
 response = openai.Completion.create(
-  engine="davinci",
-  prompt= preset,
-  temperature=0.75,
-  max_tokens=75,
-  top_p=1,
-  frequency_penalty=0.2,
-  presence_penalty=0.16,
-  stop=["###"]
+    engine="curie",
+    prompt=preset,
+    temperature=0.75,
+    max_tokens=75,
+    top_p=1,
+    frequency_penalty=0.2,
+    presence_penalty=0.16,
+    stop=["###"],
 )
 text = response.choices[0].text
 
@@ -59,18 +59,18 @@ text = response.choices[0].text
 # Limpieza del texto
 cont = 0
 for letter in text:
-  if letter == ':':
-    break
-  cont += 1
+    if letter == ":":
+        break
+    cont += 1
 
-theme = text[:cont-6]
-text = text[cont+2:len(text)-1]
+theme = text[: cont - 6]
+text = text[cont + 2 : len(text) - 1]
 text = text.capitalize()
 
 if text.find("\n") > 0:
-  text = text[:text.find("\n")]
+    text = text[: text.find("\n")]
 
-print("theme: "+theme)
+print("theme: " + theme)
 print(text)
 
 
@@ -78,8 +78,8 @@ print(text)
 
 
 def tweet(x):
-  api.update_status(x)
-  print("Tweeted")
+    api.update_status(x)
+    print("Tweeted")
+
 
 tweet(text)
-
