061bff79b1edf4a457c6813cbfcfacd7a286da72
Daniel <satelerd@gmail.com>
2021-11-17 17:30:19 -0300

    Added galia-optimized-executer.bat

---
+++ Tweet-Creation/Server-executers/galia-optimized-executer.bat
@@ -0,0 +1,15 @@
+@ECHO OFF 
+TITLE Execute python script on anaconda environment
+ECHO Please Wait...
+:: Section 1: Execute the python file
+ECHO ============================
+ECHO Python optimized_tweet_creation.py
+ECHO ============================
+python C:\Users\satel\OneDrive\code\Galia\Galia-GaPerT-III\Tweet-Creation\Python-integration\optimized_tweet_creation.py
+
+ECHO ============================
+ECHO Python optimized_tweet_creation.py 2
+ECHO ============================
+python C:\Users\satel\OneDrive\code\Galia\Galia-GaPerT-III\Tweet-Creation\Python-integration\optimized_tweet_creation.py
+
+PAUSE
