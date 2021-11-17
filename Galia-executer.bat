f837b6ba12d45998444ad1f39d82b32d8ece2eb4
Daniel <satelerd@gmail.com>
2021-11-17 10:28:01 -0300

    Updated the server bash

--- Galia-executer.bat
+++ Galia-executer.bat
@@ -5,11 +5,11 @@ ECHO Please Wait...
 ECHO ============================
 ECHO Python Tweet_Creation.py
 ECHO ============================
-python C:\Users\satel\OneDrive\code\Galia\Galia-GaPerT-III\Tweet-Creation\Tweet_Creation.py
+python C:\Users\satel\OneDrive\code\Galia\Galia-GaPerT-III\Tweet-Creation\Python integration\Tweet_Creation.py
 
 ECHO ============================
 ECHO Python Tweet_Creation.py 2
 ECHO ============================
-python C:\Users\satel\OneDrive\code\Galia\Galia-GaPerT-III\Tweet-Creation\Tweet_Creation.py
+python C:\Users\satel\OneDrive\code\Galia\Galia-GaPerT-III\Tweet-Creation\Python integration\Tweet_Creation.py
 
 PAUSE
