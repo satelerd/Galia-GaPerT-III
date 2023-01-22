2e288c96a0bbec39f5a9c64ab5bb46fe0c8731e7
Daniel <satelerd@gmail.com>
2023-01-22 17:49:08 -0300

    prompt engineering

--- Tweet-Creation/optimized_tweet_creation.py
+++ Tweet-Creation/optimized_tweet_creation.py
@@ -15,8 +15,8 @@ def gpt3_call():
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
     response = openai.Completion.create(
-        # model="text-davinci-003",
-        model="text-curie-001",
+        model="text-davinci-003",
+        # model="text-curie-001",
         prompt="Galia Gapert es una filósofa culta, creativa, de moral propia y una mujer seductora. Su humor negro resulta irritante, mordaz e irónico para el resto de gente, pero no para sus seguidores, que la encuentran original, divertida y estimulante.\n\nGalia tiene una pasión por el arte y la expresión creativa, que la llevo a un hobby peculiar, en el cual suele crear prompts. Un prompt es una breve y abstracta descripción de un concepto el cual le gustaría representar en una pintura.\nGalia se creó Twitter para compartir sus opiniones y su fin es hacer que la gente piense por sí misma. No tiene miedo de decir lo que piensa.\n\nGalia escribirá 4 generaciones de texto, en la que cada uno deberá tener un tema, un Tweet sobre el tema y una descripción para su pintura.\n\n###\nTema - Tweet - Prompt:\n1. La música - La música crea más simpatía que el sexo - una bailarina disfrutando de su baile\n2. Memes - En los 2010s los memes impulsaban la cultura, en los 2020s impulsan la economía - la ironía del cambio cultural\n3. La ironía - La ironía es la forma más honesta de decir la verdad - un político diciendo la verdad en medio de un discurso\n4. Caleidoscopio - Mi vida es un caleidoscopio de emociones que se traslapan en cada momento - una representación muy abstracta de los sentimientos\n###\nTema - Tweet - Prompt:\n1. El ocio - Siento que a veces hago mucho por la gente y la humanidad. ¿Por qué no puedo estar un rato sin hacer nada? - las dificultades sociales y el trabajo\n2. Filosofía psicodélica - La distancia es un caudal de eternidad agazapada sobre la espalda de un león - un leon agazapado pensando en la eternidad\n3. Energías renovables - Si todavía no estás pensando sobre como cambiar tu casa a energía solar... se te nota lo boomer - una casa solar en medio de la naturaleza\n4. La religión - La religión es una forma de control social que se enmascara como dogma o mentira/verdad colectiva - un hombre orando con los ojos vendados\n###\nTema - Tweet - Prompt:",
         temperature=0.8,
         max_tokens=400,
@@ -73,9 +73,8 @@ def sd_call(tweets):
 
     cont = 0
     for tweet in tweets:
-        sd_prompt = "Una pintura de " + tweet[1] + ". Arte digital"
-        sd_prompt = translator.translate(sd_prompt, src="es", dest="en").text
-        print(sd_prompt)
+        tweet = translator.translate(tweet[1], src="es", dest="en").text
+        sd_prompt = "A highly detailed matte painting titled: " + tweet + ". Volumetric lighting, 4 k resolution, masterpiece"
         answers = stability_api.generate(prompt=sd_prompt, samples=1)
 
         for rep in answers:
@@ -122,4 +121,4 @@ if __name__ == "__main__":
     response = gpt3_call()
     tweets = text_cleaning(response)
     sd_call(tweets)
-    # tweet(tweets)
+    tweet(tweets)
