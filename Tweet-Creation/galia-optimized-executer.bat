465ea0aa2af4dfcafd9b51f4f650603d4034899c
Daniel <satelerd@gmail.com>
2023-03-04 17:55:25 -0300

    names

--- Tweet-Creation/galia-optimized-executer.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF 
-TITLE Galia tweets
-ECHO Please Wait...
-
-ECHO ============================
-ECHO Python optimized_tweet_creation.py
-ECHO ============================
-python C:\Users\satel\codee\Galia-GaPerT-III\Tweet-Creation\optimized_tweet_creation.py
